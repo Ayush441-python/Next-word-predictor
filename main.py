0d6b0f3cd88994936172e57e2883186d460dacbb
AYUSH KOTHE <ayushkothe441@gmail.com>
2026-03-01 23:40:47 +0530

    Changing in main.py

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from src.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
 
-    # Step 1: Data Ingestion
+
     ingestion = DataIngestion()
     raw_data_path = ingestion.load_data()
 
-    # Step 2: Data Transformation
     transformation = DataTransformation()
     transformation.run()
 
-    # Step 3: Model Training
     trainer = ModelTrainer()
     trainer.train()
